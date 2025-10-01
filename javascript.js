49e6bad7d99b1cbd88b91718d1fb24e393aa1ea6
egordon <efgordon3@gmail.com>
2025-10-01 15:12:37 -0500

    Added table-review, added multiplication table in javascript

--- javascript.js
+++ javascript.js
@@ -1,4 +1,51 @@
-/* LOOP EXAMPLES */
+function getRowsDesired() {
+    // console.log(Number(document.getElementById('rows').value));
+    return Number(document.getElementById("rows").value)
+}
+
+function getColumnsDesired() {
+    return Number(document.getElementById("cols").value)
+}
+
+function createWithHtmlTable(rows, cols) { // method declaration, has two parameters
+    let string // define a variable called string
+    if (rows && cols) { // if rows and columns were provided
+        string = "<table>\n" // start building the HTML table
+
+        // first "for" loop to build the rows of the table
+        for (let row = 1; row <= rows; row++) {
+            string += "<tr>\n" // strat building an HTML row
+
+            // second "for" loop to build the columns of EACH row
+            for (let col = 1; col <= cols; col++) {
+                string += "<td>" // start building a single "cell" or column in the HTML row
+                string += row * col // populate the contents of the HTML cell
+                string += "</td>\n" //complete the HTML cell
+            }
+            string += "</tr>\n" // complete the HTML row
+        }
+
+        string += "</table>\n" // complete the HTML table
+    } else { // I guess the method wasn't provided either rows or columns
+        string = 'Provide some inputs' // set values of "string" variable to "Provide some inputs"
+    }
+
+    document.getElementById("output").innerHTML = string; // WRITE the string we've just built to an element
+    // in the HTML document whose ID is "output"
+}
+
+
+
+
+
+
+
+
+
+
+/*
+
+/!* LOOP EXAMPLES *!/
 console.log('Here\'s a for loop showing 1...10:\n')
 for (let i = 0 ; i <10; i++) {
     console.log(`${ i }\n`)
@@ -56,3 +103,4 @@ timesTable(4)
 timesTable(12)
 
 timesTable('x')
+*/
